dd71154be8e0f00b9ccb7d3e042dc30539113772
Fabio Bolivar Jimenez <fabiobolivarj@gmail.com>
2026-03-26 00:56:57 -0400

    Se eliminó la versión mala

--- practica2/MallP2/IGestion.cs
+++
@@ -1,9 +0,0 @@
-namespace MallProject.Interfaces
-{
-    public interface IGestion
-    {
-        void AgregarProducto(Producto producto, string nombreCategoria);
-        void EliminarProducto(Producto producto);
-        int CantidadProdByCategoria(string nombreCategoria);
-    }
-}
--- practica2/MallP2/IOperaciones.cs
+++
@@ -1,9 +0,0 @@
-using MallProject.Models;
-
-namespace MallProject.Interfaces
-{
-    public interface IOperaciones
-    {
-        void ComprarProductos(List<Producto> productos);
-    }
-}
